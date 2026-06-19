576a21e78b0776cd55f6eb2614c33cb17d6967db
Monte Coulter <monteiv@me.com>
2026-06-19 00:44:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const addTaskBtn = document.querySelector('.add-task-btn');
+const listContainer = document.getElementById('list-container');
+const taskInput = document.getElementById('input-box');
+
+const addTask = () => {
+    if (taskInput.value === '') {
+        alert('Please enter a task.');
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = taskInput.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    taskInput.value = '';
+    saveData();
+}
+
+listContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if (e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+const saveData = () => {
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+taskInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
+
+showTask();
